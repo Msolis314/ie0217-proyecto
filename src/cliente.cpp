6b5cb55119d7638d6b6b1fcd2a35ae37c20f44a6
Charlie Arroyo Sanchez <charlie.arroyo@ucr.ac.cr>
2024-06-16 11:48:27 -0600

    Implementacion de la clase cliente, primera 1.2
    Signed-off-by: Charlie Arroyo Sanchez <charlie.arroyo@ucr.ac.cr>

--- src/cliente.cpp
+++ src/cliente.cpp
@@ -10,32 +10,63 @@
 #include "cliente.hpp"
 
 
-// IMplementacion del metodo para agregar un numero de cuenta.
-void Cliente::agregarCuenta(int idCuenta){
-    sqlite3 *db;                                                        // generando puntero.
-    char *zErrMsg = 0;                                                  // mensaje de error.
-    int rc;                                                             // inicializando variable de retorno.
-
-    rc = sqlite3_open("SistemaBancario.db", &db);                       // abriendo la base de datos sistema bancario
-    if (rc){                                                            // if para verificar que se abrio correctamente
-        std::cout << "No se pudo abrir la base de datos" << std::endl;
-    }else{
-        std::cout << "Base de datos abierta con exito" << std::endl;
-    }
+Cliente::Cliente(std::string nombre, std::string apellido) {
+    this->nombre = nombre;
+    this->apellido = apellido;
     
-    int id = generarIDCuenta();            
+}
 
-   // Haciendo una consulta SQL para insertar una nueva cuenta bancaria
-    std::string sql = "INSERT INTO  CUENTA_BANCARIA(ID_CUENTA) VALUES (" + std::to_string(idCuenta) + ");";
-    const char* data = sql.c_str();                                     // Usando Sqlite
-    rc = sqlite3_exec(db, data, NULL, 0, &zErrMsg);                     // Ejecutando la consulta
-    if (rc != SQLITE_OK){                                               // si todo se ejecuta bien en el if, el cliente se agrega con exito, sino msj error
-        std::cout << "SQL ERROR: " << zErrMsg << std::endl;
-        sqlite3_free(zErrMsg);
-    }else{
-        std::cout << "Id cuenta agregada exitosamente" << std::endl;    // Numero de cuenta agregado.
-    }
+// Generando menu
 
+void Cliente::mostrarMenuC() {
+    int opc;
+    do {
+        std::cout << "------ Menú del Cliente ------" << std::endl;
+        std::cout << "1. Generar ID de cuenta" << std::endl;
+        std::cout << "2. Realizar transacciones" << std::endl;
+        std::cout << "3. Salir del menú" << std::endl;
+        std::cout << "Elija una opción: ";
+        std::cin >> opc;
+
+        switch (opc) {
+           case 1:
+                {
+                    // Submenú para elegir el tipo de cuenta del usuario
+                    int opcionTipoCuenta;
+                    do {
+                        std::cout << "Por favor, seleccione el tipo de cuenta que desea adquirir:" << std::endl;
+                        std::cout << "1. Cuenta en dólares" << std::endl;
+                        std::cout << "2. Cuenta en colones" << std::endl;
+                        std::cout << "Elija una opción: ";
+                        std::cin >> opcionTipoCuenta;
+
+                        if (opcionTipoCuenta == 1) {
+                            int id_newD;
+                            agregarCuentaD(id_newD);
+                            std::cout << "Cuenta exitosamente agregada en dólares." << std::endl;
+
+                        } else if (opcionTipoCuenta == 2) {
+                            int id_newC;
+                            agregarCuentaC(id_newC);
+                            std::cout << "Cuenta exitosamente agregada en colones." << std::endl;
+                        } else {
+                            std::cout << "Opción inválida. Intente de nuevo." << std::endl;
+                        }
+                    } while (opcionTipoCuenta != 1 && opcionTipoCuenta != 2);
+                }
+                break;
+            case TRANS:
+                std::cout << "**********************************************" << std::endl;
+                std::cout << "*** Bienvenida al sistema de transacciones.***" << std::endl;
+                std::cout << "**********************************************" << std::endl;
+                break;
+            case SALIRMENUC:
+                std::cout << "Saliendo del menú del cliente..." << std::endl;
+                break;
+            default:
+                std::cout << "Opción inválida. Intente de nuevo." << std::endl;
+        }
+    } while (opc != SALIRMENUC);
 }
 
 
@@ -44,31 +75,129 @@ void Cliente::agregarCuenta(int idCuenta){
 
 
 
-// generando el numero de cuenta del cliente.
-int Cliente::generarIDCuenta(){
-    // Generar un número aleatorio que funcione como un id unico
-    int idCuenta = rand() * (RAND_MAX + 1) + rand();
+// IMplementacion del metodo para agregar un numero de cuenta.
+void Cliente::agregarCuentaC(int idCuentaC) {
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open("SistemaBancario.db", &db);
+    if (rc) {
+        std::cout << "No se pudo abrir la base de datos" << std::endl;
+        return;
+    }
+
+    // Generar un nuevo ID de cuenta
+    int id_cuentaC = generarIDCuenta();
+
+    // Preparar la consulta SQL para insertar el ID de cuenta en colones
+    std::string sql = "INSERT INTO CUSTOMERS(ID_CUENTA_C) VALUES (" + std::to_string(id_cuentaC) + ");";
+    const char* data = sql.c_str();
+
+    // Ejecutar la consulta SQL
+    rc = sqlite3_exec(db, data, NULL, 0, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        std::cout << "Error al insertar el ID de cuenta en colones: " << zErrMsg << std::endl;
+        sqlite3_free(zErrMsg);
+    } else {
+        std::cout << "ID de cuenta en colones agregado exitosamente." << std::endl;
+    }
+
+    sqlite3_close(db);
+}
+
+
+// IMplementacion del metodo para agregar un numero de cuenta.
+void Cliente::agregarCuentaD(int idCuentaD) {
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open("SistemaBancario.db", &db);
+    if (rc) {
+        std::cout << "No se pudo abrir la base de datos" << std::endl;
+        return;
+    }
+
+    // Generar un nuevo ID de cuenta
+    int id_cuentaD = generarIDCuenta();
+
+    // Preparar la consulta SQL para insertar el ID de cuenta en colones
+    std::string sql = "INSERT INTO CUSTOMERS(ID_CUENTA_D) VALUES (" + std::to_string(id_cuentaD) + ");";
+    const char* data = sql.c_str();
+
+    // Ejecutar la consulta SQL
+    rc = sqlite3_exec(db, data, NULL, 0, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        std::cout << "Error al insertar el ID de cuenta en dolares: " << zErrMsg << std::endl;
+        sqlite3_free(zErrMsg);
+    } else {
+        std::cout << "ID de cuenta en dolares agregado exitosamente." << std::endl;
+    }
+
+    sqlite3_close(db);
+}
+
+
+
+
+int Cliente::generarIDCuenta() {
+// definiendo un rango
+    long int minDigits = 1000000000000000; // si queremos que contenga al menos 16 digitos 
+    long int maxDigits = 9999999999999999;  
+
+    int idnum = minDigits + rand() % (maxDigits - minDigits + 1); // generamos un numero aleatorio y tomamos el resto del modulo entero
 
     // Verificar si el ID generado ya existe en la base de datos
-    while (checkIDCuentaExists(idCuenta)) {
+    while (checkIDCuentaExists(idnum)) {
         // Si el ID ya existe, generamos otro número aleatorio y lo volvemos a probar
-        idCuenta = rand() * (RAND_MAX + 1) + rand();
+        idnum = minDigits + rand() % (maxDigits - minDigits + 1);
     }
-
-    return idCuenta;
+    return idnum;
 }
-
 
 
 
 bool Cliente::checkIDCuentaExists(int idCuenta) {
-    std::vector<int>::iterator it;
-    // Recorre la lista de IDs de cuentas y verifica si alguno coincide con el ID proporcionado.
-    for (it = .begin(); it != actualIDCuentas.end(); it++) {                                  // FALTA DEFINIR COSAS
-        if (*it == idCuenta) {
-            return true; // Si encuentra un ID igual, devuelve true.
-        }
+    sqlite3 *db;
+    int rc;                                           // verificador de que la todo funciona
+    char* zErrMsg = 0;                                // Manejo de errores
+    bool found = false;                               // generar una variable que me indique si se encontro en la base de datos
+
+    rc = sqlite3_open("SistemaBancario.db", &db);
+    if (rc) {
+        std::cout << "No se pudo abrir la base de datos" << std::endl;
+        return false;
+    } else {
+        std::cout << "Base de datos abierta con exito" << std::endl;
     }
-    return false; // Si no se encuentra ningún ID igual, devuelve false.
+    
+    std::string sql = "SELECT COUNT(*) FROM CUSTOMERS WHERE ID_CUENTA_C = " + std::to_string(idCuenta); // se va buscar en el parametro que se le pase
+
+    rc = sqlite3_exec(db, sql.c_str(), [](void* foundPtr, int argc, char** argv, char** azColName) -> int {
+        bool* found = static_cast<bool*>(foundPtr);                                                             // 
+        *found = (argc > 0 && std::stoi(argv[0]) > 0); // Verifica si se encontró alguna fila y si el valor de COUNT(*) es mayor que 0
+        return 0;                                      // si todo bien, retorna cero
+    }, &found, &zErrMsg);                              
+
+    if (rc != SQLITE_OK) {                                                                 
+        std::cerr << "Error en la consulta SQL: " << zErrMsg << std::endl; // mensaje de error
+        sqlite3_free(zErrMsg);                          // liberar memoria            
+    }
+
+    return found;                                      // devuelve true si se encontro en la base de datos, fase si no
 }
 
+
+
+void Cliente::agregarPrestamo(int idPrestamo) {
+    // xxxx
+}
+
+void Cliente::consultarCuentas() {
+    // xxxx
+}
+
+void Cliente::consultarPrestamos() {
+    // xxxx
+}
--- src/cliente.hpp
+++ src/cliente.hpp
@@ -5,6 +5,18 @@
 #include <vector>
 #include "entidadBancaria.hpp"
 
+
+
+
+int menu_cliente();
+enum MenuOptionsC{
+    GEN_ID_CUENTA = 1,
+    TRANS = 2,
+    SALIRMENUC = 3
+};
+
+
+
 class Cliente: public EntidadBancaria{
     private:
         std::string nombre;
@@ -14,12 +26,14 @@ class Cliente: public EntidadBancaria{
         std::vector<int> prestamos;
     public:
         Cliente(std::string nombre, std::string apellido);
-        void agregarCuenta(int idCuenta);
+        void agregarCuentaC(int idCuentaC);
+        void agregarCuentaD(int idCuentaD);
         void agregarPrestamo(int idPrestamo);
         void consultarCuentas();
         void consultarPrestamos();
         int generarIDCuenta();
         bool checkIDCuentaExists(int idCuenta); 
+        void mostrarMenuC();
         // void transaccion(float monto, Monedas moneda);
         // void operarPrestamo(float monto, Monedas moneda);
 
